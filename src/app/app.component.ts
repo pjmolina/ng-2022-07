a9b18c3be2faa80dc542265e1f1a0985c35431f8
Pedro J. Molina <pjmolina@gmail.com>
2022-07-18 10:04:24 +0200

    *ngFor ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,13 @@ export class AppComponent {
   title = 'angular0';
   nombreSeleccionado?: string;
 
+  usuarios: UserData[] = [
+    { name: 'Maria', surname: 'Garcia' },
+    { name: 'Ana', surname: 'Lopez' },
+    { name: 'Pepe', surname: 'Gutierrez' },
+    { name: 'Pablo', surname: 'Molina' },
+  ];
+
   usuarioSeleccionado(user: UserData): void {
     // this.nombreSeleccionado = user.name + ' ' + user.surname;
     this.nombreSeleccionado = `${user.name} ${user.surname}`;
